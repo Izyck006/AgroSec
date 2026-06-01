1548816dc75f5f08f1a1cb60221e94c3dbcc90ba
Izyck006 <ehimenaudu56@gmail.com>
2026-06-01 15:29:22 +0100

    more time

--- Start_AgroSec.bat
+++ Start_AgroSec.bat
@@ -8,15 +8,15 @@ echo [1/3] Launching Spring Boot Backend...
 :: CHANGE THE PATH BELOW TO YOUR ACTUAL BACKEND FOLDER
 start cmd /k "cd /d C:\Users\T470\Downloads\Project\backend && .\mvnw.cmd spring-boot:run"
 
-:: Wait 15 seconds to give the Java server enough time to boot up completely
-timeout /t 15
+:: Wait 20 seconds to give the Java server enough time to boot up completely
+timeout /t 20
 
 echo [2/3] Launching Permanent Ngrok Tunnel...
 :: This uses your permanent Ngrok link automatically
 start cmd /k "ngrok http --domain=reproach-sinner-femur.ngrok-free.dev 8080"
 
-:: Wait 10 seconds for the tunnel to establish connection
-timeout /t 10
+:: Wait 15 seconds for the tunnel to establish connection
+timeout /t 15
 
 echo [3/3] Launching Python Edge AI Camera Node...
 :: CHANGE THE PATH BELOW TO WHERE YOUR PYTHON SCRIPT IS
