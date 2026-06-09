c068888755ee5ce92582e43fef172a56cccf4c43
Izyck006 <ehimenaudu56@gmail.com>
2026-06-09 14:56:35 +0100

    backend issue

--- backend/src/main/java/com/farmsecurity/backend/controller/AlertController.java
+++ backend/src/main/java/com/farmsecurity/backend/controller/AlertController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import com.farmsecurity.backend.model.Alert;
 import com.farmsecurity.backend.repository.AlertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/alerts")
@@ -14,17 +16,17 @@ public class AlertController {
     @Autowired
     private AlertRepository alertRepository;
 
-     @PostMapping
-    public Alert createAlert(@RequestBody Alert alert) {
-        return alertRepository.save(alert);
-    }
-
+    // 1. The React Dashboard uses this to GET the data
     @GetMapping
-    public List<Alert> getAllAlerts(){
+    public List<Alert> getAllAlerts() {
         List<Alert> allAlerts = alertRepository.findAll();
         Collections.reverse(allAlerts);
         return allAlerts.stream().limit(20).collect(Collectors.toList());
     }
 
-   
+    // 2. The Python Edge Node uses this to POST new alerts
+    @PostMapping
+    public Alert createAlert(@RequestBody Alert alert) {
+        return alertRepository.save(alert);
+    }
 }
