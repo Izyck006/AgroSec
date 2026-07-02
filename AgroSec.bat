0baa844ee08eb2d3e140bc1cd22964336689b598
Izyck006 <ehimenaudu56@gmail.com>
2026-07-02 09:48:22 +0100

    Made sig changes

--- AgroSec.bat
+++ AgroSec.bat
@@ -1,23 +1,23 @@
 @echo off
 title AgroSec Automation
 echo ===================================================
-echo              STARTING AGROSEC
+echo              STARTING AGROSEC PRODUCT
 echo ===================================================
 
-echo [1/3] Launching Spring Boot Backend...
+echo [1/3] Start Spring Boot Backend.
 
 start cmd /k "cd /d C:\Users\T470\Downloads\Project\backend && .\mvnw.cmd spring-boot:run"
 
 timeout /t 25
 
-echo [2/3] Launching Ngrok Server Tunnel...
+echo [2/3] Ngrok Server Tunnel:
 
 start cmd /k "ngrok http --domain=reproach-sinner-femur.ngrok-free.dev 8080"
 
 
 timeout /t 18
 
-echo [3/3] Launching Python Edge AI Camera...
+echo [3/3] Run Python node and Camera...
 
 start cmd /k "cd /d C:\Users\T470\Downloads\Project && python edge_detector.py"
 
