03147124a777d485764809ab7345b09661a142b1
Izyck006 <ehimenaudu56@gmail.com>
2026-07-23 21:23:39 +0100

    made my batch script

--- AgroSec.bat
+++ AgroSec.bat
@@ -11,7 +11,7 @@ start cmd /k "ngrok http --domain=reproach-sinner-femur.ngrok-free.dev 8080"
 
 timeout /t 18
 
-echo [1/2] Run Python node and Camera...
+echo [1/2] Run Python node and start the Camera:
 
 start cmd /k "cd /d C:\Users\T470\Downloads\Project && python edge_detector.py"
 
